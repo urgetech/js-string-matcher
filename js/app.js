4ba660810bda9dc2baae1785109a87ba6ac730c5
RJ Kirkland <rckirkland@wccnet.edu>
2017-02-06 15:37:05 -0500

    Make functions and variables local
    not global;
    also commented on logical errors.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,33 @@
-function getStringMatches (firstString, secondString){
-    this.searchParams = parseSearchParams(firstString);
-    this.matches = 0;
+var myComparisonObject = 
+{
+    getStringMatches: function(firstString, secondString)
+    {
+        this.searchParams = this.parseSearchParams(firstString);
+        this.matches = 0;
 
-    var currentParam = '';
-    for (var index in searchParams){
-        currentParam = searchParams[index];
-        if ( secondString.search(currentParam) != -1 ){
-            this.matches ++;
+        var currentParam = '';
+        for (var index in this.searchParams)
+        {
+            currentParam = this.searchParams[index];
+            if (secondString.search(currentParam) != -1) // Error: secondString.search is looking for CHARACTER matches not FIELD matches
+            {
+                this.matches++;
+            }
         }
+        return this.matches;
+    },
+    parseSearchParams: function(queryString)
+    {
+        return queryString.split('/');
+    },
+    compare: function(a , b)
+    {
+        this.count = this.getStringMatches(a, b)
+        return this.count/(b.length + a.length); // Error: this.count is in FIELDS and b.length is in CHARACTERS
     }
-    return this.matches;
-}
-
-function parseSearchParams(queryString){
-     return queryString.split('/');
-}
-
-function compare(a , b) {
-    this.count = getStringMatches (a, b)
-    return this.count/(b.length + a.length);
-}
+};
 
 var a = 'term/course';
-var b = 'term/course/';
+var b = 'term/course';
 
-console.log(compare(a,b));
+console.log(myComparisonObject.compare(a,b));
